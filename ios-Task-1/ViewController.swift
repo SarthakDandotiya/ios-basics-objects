947bea8daf2b3f043c51824c30eafea2a7a5bcd5
SarthakDandotiya <sarthak.dandotiya23@gmail.com>
2019-05-16 22:34:50 +0530

    Initial Commit

---
+++ ios-Task-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-Task-1
+//
+//  Created by Sarthak Dandotiya on 16/05/19.
+//  Copyright © 2019 Sarthak Dandotiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
